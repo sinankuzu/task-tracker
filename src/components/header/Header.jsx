5d885818c071962742286f0951b812a13a258dd6
Sinan <sinankzu@gmail.com>
2022-10-25 10:12:39 +0200

    aa

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+        <h1>Task Tracker</h1>
+        <button>Show Add Task Bar</button>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/section/Section.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Section = () => {
+  return (
+    <section>
+    <div>No tasks to show</div>
+    </section>
+  )
+}
+
+export default Section
