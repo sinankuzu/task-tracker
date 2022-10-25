6b25c2153c31e017d4c04b8bbd44681b42636f82
Sinan <sinankzu@gmail.com>
2022-10-25 20:34:01 +0200

    ss

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
 import Header from "./components/header/Header";
 import Section from "./components/section/Section";
+import Taskbar from "./components/section/Taskbar";
 
 function App() {
   return (
     <div className="container">
       <div className="center">
         <Header />
-
+        {false && <Taskbar/>}
         <Section />
       </div>
     </div>
